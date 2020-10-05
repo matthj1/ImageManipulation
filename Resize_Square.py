adc03c79e9ebcdfa3b108ea5892f3789385d3f42
Joe Matthews <joematthewsphotography@gmail.com>
2020-10-05 11:47:09 +0100

    tidied up a bit

--- Resize_Square.py
+++ Resize_Square.py
@@ -2,15 +2,16 @@ from PIL import Image
 
 image = Image.open("Original/Bella.jpg")
 
-max_dimension = 255
+max_dimension = 125
 
 size_x, size_y = image.size
 
 if size_x > size_y:
     scale = size_y/max_dimension
-    new_dimensions = (size_x//scale, size_y//scale)
-    image.thumbnail(new_dimensions)
-    print(new_dimensions)
-    print(((new_dimensions[0]-max_dimension)//2, 0, max_dimension + (new_dimensions[0]-max_dimension)//2, max_dimension))
-    new = image.crop(((new_dimensions[0]-max_dimension)//2, 0, max_dimension + (new_dimensions[0]-max_dimension)//2, max_dimension))
-    new.show()
+else:
+    scale = size_x/max_dimension
+
+new_dimensions = (size_x//scale, size_y//scale)
+image.thumbnail(new_dimensions)
+new = image.crop(((new_dimensions[0]-max_dimension)//2, 0, max_dimension + (new_dimensions[0]-max_dimension)//2, max_dimension))
+new.show()
